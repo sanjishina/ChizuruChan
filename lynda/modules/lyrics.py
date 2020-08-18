552ec733d07859d5a824774b871fce860ccf7522
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-08-18 09:07:36 +0800

    Update lyrics.py

--- lynda/modules/lyrics.py
+++ lynda/modules/lyrics.py
@@ -5,8 +5,8 @@ from tswift import Song
 from telegram import Bot, Update, Message, Chat
 from telegram.ext import run_async
 
-from stella import dispatcher
-from stella.modules.disable import DisableAbleCommandHandler
+from lynda import dispatcher
+from lynda.modules.disable import DisableAbleCommandHandler
 
 
 @run_async

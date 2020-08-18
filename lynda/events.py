ebe5be1ffd54696bbe7dc75c67ee79bea84d52cd
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-08-18 10:01:36 +0800

    Update events.py

--- lynda/events.py
+++ lynda/events.py
@@ -16,7 +16,7 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 from telethon import events
-from lynda import tbot
+from lynda import telethn
 
 
 def register(**args):

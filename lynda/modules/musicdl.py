08c432ce584789794d1f6afbbe2ecd2694797c9d
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-09-03 18:34:39 +0800

    Update and rename music.py to musicdl.py

--- lynda/modules/musicdl.py
+++ lynda/modules/musicdl.py
@@ -5,17 +5,14 @@ from telegram.ext import CommandHandler, run_async, Filters
 import requests, logging
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 from telegram import Message, Chat, Update, Bot, MessageEntity
-from lynda import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS, BAN_STICKER
+from lynda import dispatcher, OWNER_ID, SUDO_USERS, SUPPORT_USERS, WHITELIST_USERS
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
-from lynda.modules.helper_funcs.chat_status import user_admin, sudo_plus
-
+from lynda.modules.helper_funcs.chat_status import user_admin
 
 count = 0
-
-@sudo_plus
+@run_async
 def music(bot: Bot, update: Update, args):
 	message = update.effective_message
-	msg_id = update.effective_message.message_id
 	global count
 
 	chatId = update.message.chat_id
@@ -25,18 +22,19 @@ def music(bot: Bot, update: Update, args):
 	if video_id.find('youtu.be') != -1:
 		index = video_id.rfind('/') + 1
 		video_id = video_id[index:][:11]
-		bot.send_message(chat_id=chatId, text='Please wait...!\nstella downloading audio file.',reply_to_message_id=msg_id)
+		message.reply_text("Please wait...\ndownloading audio.")
 
 	elif video_id.find('youtube') != -1:
 		index = video_id.rfind('?v=') + 3
 		video_id = video_id[index:][:11]
-		bot.send_message(chat_id=chatId, text='Please wait...!\nstella downloading audio file.',reply_to_message_id=msg_id)
+		message.reply_text("Please wait...\downloading audio.")
 
 	elif not video_id.find('youtube') != -1:
-		bot.send_message(chat_id=chatId, text="It's not seems link youtube link.\nProvide me youtube link.",reply_to_message_id=msg_id)
+		message.reply_text("Please provide me youtube link")
 
 	elif not video_id.find('youtu.be') != -1:
-		bot.send_message(chat_id=chatId, text="It's not seems link youtube link.\nProvide me youtube link.",reply_to_message_id=msg_id)		
+		message.reply_text("Please provide me youtube link")
+		
 
         
 
@@ -60,22 +58,22 @@ def music(bot: Bot, update: Update, args):
 		if not json2_response['error']:
 			payload = json2_response['payload']
 
-			info = '*{0}*'.format(payload['fulltitle'])
+			info = '*{0}* \nUploaded by @AnonymousD3061'.format(payload['fulltitle'])
 
 			try:
 				
-				bot.send_audio(chat_id=chatId, audio=json2_response['url'] ,parse_mode='Markdown',text="meanya", caption=info, 	reply_to_message_id=msg_id)
+				bot.send_audio(chat_id=chatId, audio=json2_response['url'] ,parse_mode='Markdown',text="meanya", caption=info)
 				count += 1
 				print("\033[1m\033[96m" + "Download count: " + str(count) + "\033[0m")
 			except:
-				bot.send_message(chat_id=chatId, text='Something went wrong with the download..!\nPlease Report there @spookyenvy',reply_to_message_id=msg_id)
+				bot.send_message(chat_id=chatId, text='Something went wrong with the download..!\nPlease Report there @AnonymousD3061')
 
 
+__help__ = """ Youtube audio Downloader
+ - /music <Youtube link> : download audio file from youtube link.
 
-__help__ = """ Youtube Audio Downloader
- - /music <Youtube link> : chizuru can download audio file from youtube link.
 """
-__mod_name__ = "Music" 
+__mod_name__ = "Music Dl" 
 
 music_handler = CommandHandler('music', music, pass_args=True)
 dispatcher.add_handler(music_handler)

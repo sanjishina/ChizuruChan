743dc34c84fca366b3aac3b965ffb74bcfc6f511
Muhammad Nur Fauzan <60289157+Yashiro-San@users.noreply.github.com>
2020-09-21 23:01:07 +0800

    Update string_handling.py

--- lynda/modules/helper_funcs/string_handling.py
+++ lynda/modules/helper_funcs/string_handling.py
@@ -256,3 +256,13 @@ def extract_time(message, time_val):
     else:
         message.reply_text("Invalid time type specified. Expected m,h, or d, got: {}".format(time_val[-1]))
         return ""
+      
+def markdown_to_html(text):
+    text = text.replace("*", "**")
+    text = text.replace("`", "```")
+    text = text.replace("~", "~~")
+    _html = markdown2.markdown(text, extras=["strike", "underline"])
+    return bleach.clean(
+        _html, tags=["strong", "em", "a", "code", "pre", "strike", "u"], strip=True
+    )[:-1]
+  
